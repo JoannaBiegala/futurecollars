ee8b361ae8bb4d2d3928c98f3933afaa10e49216
Joanna Biegala <joannabiegala0511@gmail.com>
2022-06-01 19:43:21 +0200

    Lesson 4 - OOP 2 - Activity 4 - Uppercase and lowercase letters

--- src/main/java/com/futurecollars/lesson7/MinutesToSecondsConverter.java
+++ src/main/java/com/futurecollars/lesson7/MinutesToSecondsConverter.java
@@ -1,9 +1,6 @@
 package com.futurecollars.lesson7;
 
-public class ConvertMinutesToSeconds {
-    public ConvertMinutesToSeconds() {
-    }
-
+public class MinutesToSecondsConverter {
     public static int getSecondsFromGivenMinutes(int minutes) {
         return minutes * 60;
     }
