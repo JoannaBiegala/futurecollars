d1438d5ca8c24ee2179e98271378273cdefa1b39
Joanna Biegala <joannabiegala0511@gmail.com>
2022-06-03 12:34:15 +0200

    initial commit

---
+++ src/main/java/com/futurecollars/lesson8/Person.java
@@ -0,0 +1,30 @@
+package com.futurecollars.lesson8;
+
+import java.util.Objects;
+
+public class Person {
+    String firstName;
+    String lastName;
+    String occupation;
+    int yearOfBirth;
+
+    public Person(String firstName, String lastName, String occupation, int yearOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.occupation = occupation;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return yearOfBirth == person.yearOfBirth && firstName.equals(person.firstName) && lastName.equals(person.lastName) && occupation.equals(person.occupation);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, occupation, yearOfBirth);
+    }
+}
