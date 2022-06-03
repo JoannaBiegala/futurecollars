a3ba8326d35d43ea77c0be133022779b7dfd2d7e
Joanna Biegala <joannabiegala0511@gmail.com>
2022-06-03 15:42:07 +0200

    initial commit

---
+++ src/main/java/com/futurecollars/lesson8/Triangle.java
@@ -0,0 +1,14 @@
+package com.futurecollars.lesson8;
+
+public class Triangle {
+    double width;
+    double height;
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
+}
