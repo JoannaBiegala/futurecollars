8730ddb352492febde862021d244ad9c44615db8
Joanna Biegala <joannabiegala0511@gmail.com>
2022-06-07 16:54:31 +0200

    small changes

--- src/main/java/com/futurecollars/lesson8/daysOfWeek/NameOfTheDay.java
+++ src/main/java/com/futurecollars/lesson8/daysOfWeek/NameOfTheDay.java
@@ -5,4 +5,5 @@ public class NameOfTheDay {
     String getNameOfTheDay(DaysOfWeek daysOfWeek) {
         return String.valueOf(daysOfWeek);
     }
+
 }
--- src/test/java/com/futurecollars/lesson8/daysOfWeek/NameOfTheDayTest.java
+++ src/test/java/com/futurecollars/lesson8/daysOfWeek/NameOfTheDayTest.java
@@ -3,16 +3,15 @@ package com.futurecollars.lesson8.daysOfWeek;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
 class NameOfTheDayTest {
-
     @Test
     void formatText() {
         //given
         DaysOfWeek sunday = DaysOfWeek.valueOf("Sunday");
         //when
-        String expectSunday = new NameOfTheDay().getNameOfTheDay(sunday);
+        String shouldBeSunday = new NameOfTheDay().getNameOfTheDay(sunday);
         //then
-        Assertions.assertEquals("Sunday", expectSunday);
+        Assertions.assertEquals("Sunday", shouldBeSunday);
     }
+
 }
