bbdfa5044d8a638c104f8755b0aff9b21772f9bb
Joanna Biegala <joannabiegala0511@gmail.com>
2022-06-13 11:48:32 +0200

    initial commit

---
+++ src/main/java/com/futurecollars/lesson10/everySecondLine/EverySecondLineWriter.java
@@ -0,0 +1,25 @@
+package com.futurecollars.lesson10.everySecondLine;
+
+import java.io.*;
+
+public class EverySecondLineWriter {
+
+    void writeEverySecondLine(String path) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+            String line;
+            int numberLine = 1;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (numberLine % 2 == 0) {
+                    System.out.println(line);
+                }
+                numberLine++;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File " + path + " does not exist");
+        } catch (IOException e) {
+            System.out.println("IOException");
+        }
+    }
+
+}
---
+++ src/main/java/com/futurecollars/lesson10/everySecondLine/Main.java
@@ -0,0 +1,9 @@
+package com.futurecollars.lesson10.everySecondLine;
+
+public class Main {
+
+        public static void main(String[] args) {
+             new EverySecondLineWriter().writeEverySecondLine("src/main/java/com/futurecollars/lesson10/everySecondLine/data.txt");
+         }
+
+}
