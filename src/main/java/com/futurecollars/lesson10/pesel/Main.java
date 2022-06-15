25691831fa48469b8c1606ffc1c50e28ba43b98f
Joanna Biegala <joannabiegala0511@gmail.com>
2022-06-15 09:54:11 +0200

    refactoring code

--- src/main/java/com/futurecollars/lesson10/pesel/Main.java
+++ src/main/java/com/futurecollars/lesson10/pesel/Main.java
@@ -3,10 +3,9 @@ package com.futurecollars.lesson10.pesel;
 public class Main {
 
     public static void main(String[] args) {
-
-        if ( new PeselValidator().validatePesel()) {
+        if (new PeselValidator().validatePesel()) {
             System.out.println("Pesel is correct");
         }
-
     }
+
 }
--- src/main/java/com/futurecollars/lesson10/pesel/PeselValidator.java
+++ src/main/java/com/futurecollars/lesson10/pesel/PeselValidator.java
@@ -1,42 +1,21 @@
 package com.futurecollars.lesson10.pesel;
 
-
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class PeselValidator {
 
     boolean validatePesel() {
-
         Scanner scanner = new Scanner(System.in);
         System.out.print("Insert PESEL: ");
         String pesel = scanner.nextLine();
-        if (!Pattern.matches("[0-9]+",pesel)) {
-             throw new WrongTypeOfDataException(pesel);
+        if (!Pattern.matches("[0-9]+", pesel)) {
+            throw new WrongTypeOfDataException(pesel);
         }
         if (pesel.length() != 11) {
             throw new IllegalLengthException(pesel.length());
-         }
-
+        }
         return true;
     }
-
-    private static class IllegalLengthException extends IllegalArgumentException {
-
-        public IllegalLengthException(int length) {
-            super("IllegalLengthException{" +
-                    "length=" + length + "}");
-        }
-    }
-
-    private static class WrongTypeOfDataException extends IllegalArgumentException{
-
-       public WrongTypeOfDataException(String text) {
-            System.out.println("Input text should be contains only digits {" +
-                     text + '}');
-
-        }
-
-     }
 
 }
