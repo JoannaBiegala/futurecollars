f049c9a9a308250cfca8e73c54f26f4d5f216ce2
Joanna Biegala <joannabiegala0511@gmail.com>
2022-06-21 10:43:14 +0200

    refactoring code

--- src/main/java/com/futurecollars/lesson5/task4/Employee.java
+++ src/main/java/com/futurecollars/lesson5/task4/Employee.java
@@ -3,6 +3,7 @@ package com.futurecollars.lesson5.task4;
 import java.math.BigDecimal;
 
 public class Employee {
+
     private final String name;
     private final String surname;
     private int age;
