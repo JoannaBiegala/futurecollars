e64b1ce6d46aa84aefe8a7436a013ab4cb5c3cf8
Joanna Biegala <joannabiegala0511@gmail.com>
2022-06-23 07:52:57 +0200

    refactoring code

--- src/main/java/com/futurecollars/lesson10/squareRoot/SquareRootCalculator.java
+++ src/main/java/com/futurecollars/lesson10/squareRoot/SquareRootCalculator.java
@@ -2,7 +2,7 @@ package com.futurecollars.lesson10.squareRoot;
 
 public class SquareRootCalculator {
 
-    double calculateSquareRoot(double number) {
+    static double calculateSquareRoot(double number) {
         if (number < 0) {
             throw new IllegalArgumentException("Number must be >= 0: " + number);
         }
