1f3714acf70dab87c748d0f30595dea3fbafdc4e
Joanna Biegala <joannabiegala0511@gmail.com>
2022-06-23 08:42:50 +0200

    refactoring code

--- src/main/java/com/futurecollars/lesson10/squareRoot/Main.java
+++ src/main/java/com/futurecollars/lesson10/squareRoot/Main.java
@@ -3,9 +3,9 @@ package com.futurecollars.lesson10.squareRoot;
 public class Main {
 
     public static void main(String[] args) {
-        double squereRootOfNumber = new SquareRootCalculator().calculateSquareRoot(4.0);
+        double squereRootOfNumber = SquareRootCalculator.calculateSquareRoot(4.0);
         System.out.println("Result square root: " + squereRootOfNumber);
-        squereRootOfNumber = new SquareRootCalculator().calculateSquareRoot(-1.0);
+        squereRootOfNumber = SquareRootCalculator.calculateSquareRoot(-1.0);
         System.out.println("Result square root:: " + squereRootOfNumber);
 
     }
--- src/main/java/com/futurecollars/lesson10/squareRoot/SquareRootCalculator.java
+++ src/main/java/com/futurecollars/lesson10/squareRoot/SquareRootCalculator.java
@@ -2,7 +2,7 @@ package com.futurecollars.lesson10.squareRoot;
 
 public class SquareRootCalculator {
 
-    static double calculateSquareRoot(double number) {
+    public static double calculateSquareRoot(double number) {
         if (number < 0) {
             throw new IllegalArgumentException("Number must be >= 0: " + number);
         }
