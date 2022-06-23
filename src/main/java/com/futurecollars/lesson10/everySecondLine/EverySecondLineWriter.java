af4c156a6259ea86318178612f7e4faa612593be
Joanna Biegala <joannabiegala0511@gmail.com>
2022-06-23 09:25:54 +0200

    refactoring code

--- src/main/java/com/futurecollars/lesson10/everySecondLine/EverySecondLineWriter.java
+++ src/main/java/com/futurecollars/lesson10/everySecondLine/EverySecondLineWriter.java
@@ -1,25 +1,18 @@
 package com.futurecollars.lesson10.everySecondLine;
 
 import java.io.*;
-import java.util.Arrays;
 
 public class EverySecondLineWriter {
 
-    void writeEverySecondLine(String path) {
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
-            String line;
-            int numberLine = 1;
-            while ((line = bufferedReader.readLine()) != null) {
-                if (numberLine % 2 == 0) {
-                    System.out.println(line);
-                }
-                numberLine++;
+    public static void writeEverySecondLine(String path) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+        String line;
+        int numberLine = 1;
+        while ((line = bufferedReader.readLine()) != null) {
+            if (numberLine % 2 == 0) {
+                System.out.println(line);
             }
-        } catch (FileNotFoundException e) {
-            System.out.println("File " + path + " does not exist");
-        } catch (IOException e) {
-            System.out.println("IOException: " + Arrays.toString(e.getStackTrace()));
+            numberLine++;
         }
     }
 
