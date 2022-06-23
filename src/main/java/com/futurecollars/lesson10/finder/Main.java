94141878118ef4441a303ad4f9badd7aaffc718a
Joanna Biegala <joannabiegala0511@gmail.com>
2022-06-23 09:01:18 +0200

    refactoring code

--- src/main/java/com/futurecollars/lesson10/finder/Main.java
+++ src/main/java/com/futurecollars/lesson10/finder/Main.java
@@ -1,14 +1,20 @@
 package com.futurecollars.lesson10.finder;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
         String word = "Pliki";
-        boolean isContained =  new WordFinderInFile().findWordInFile("src/main/java/com/futurecollars/lesson10/finder/data.txt",word);
-        if (isContained) {
-            System.out.println("The word \"" + word + "\" appears in the file");
-        } else {
-            System.out.println("The word \"" + word + "\" not appears in the file");
+        try {
+            if (WordFinderInFile.findWordInFile("src/main/java/com/futurecollars/lesson10/finder/data.txt", word)) {
+                System.out.println("The word \"" + word + "\" appears in the file");
+            } else {
+                System.out.println("The word \"" + word + "\" not appears in the file");
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
--- src/main/java/com/futurecollars/lesson10/finder/WordFinderInFile.java
+++ src/main/java/com/futurecollars/lesson10/finder/WordFinderInFile.java
@@ -1,24 +1,19 @@
 package com.futurecollars.lesson10.finder;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class WordFinderInFile {
 
-    boolean findWordInFile(String path, String word) {
-        String text = "";
+    public static boolean findWordInFile(String path, String word) throws IOException {
         File file = new File(path);
         if (!file.exists()) {
-            System.out.println("File " + path + " does not exist");
-            return false;
+            throw new FileNotFoundException("File " + path + " does not exist");
         }
-        try {
-           text = Files.readString(Path.of(path));
-        } catch (IOException e) {
-            e.getStackTrace();
-        }
+        String text = Files.readString(Path.of(path));
         return text.contains(word);
     }
 
