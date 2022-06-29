6e109f35249022df4e4565658014b7a380ca3f64
Joanna Biegala <joannabiegala0511@gmail.com>
2022-06-29 08:59:10 +0200

    use @MethodSource

--- src/test/java/com/futurecollars/lesson11/lenghtOfText/lengthOfText/LengthCalculatorTest.java
+++ src/test/java/com/futurecollars/lesson11/lenghtOfText/lengthOfText/LengthCalculatorTest.java
@@ -2,18 +2,30 @@ package com.futurecollars.lesson11.lenghtOfText.lengthOfText;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 class LengthCalculatorTest {
 
-    @ParameterizedTest
-    @CsvSource(value = {"Ala:3", "Kotek:5", ":0"}, delimiter = ':')
+    @ParameterizedTest(name = "text: \"{0}\", length: {1}")
+    @MethodSource(value = "getParams")
     void shouldBeLengthOfText(String text, int expected) {
         //when
         int lengthOfText = new LengthCalculator().getLengthOfText(text);
         //then
         Assertions.assertEquals(expected, lengthOfText);
 
+    }
+
+    private static Stream<Arguments> getParams() {
+        return Stream.of(
+                Arguments.of("Ala", 3),
+                Arguments.of("Kotek", 5),
+                Arguments.of("", 0)
+        );
     }
 
 }
