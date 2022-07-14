c1cd660a02bbc988093de9546b8ecba8af9ba53d
Joanna Biegala <joannabiegala0511@gmail.com>
2022-07-14 20:27:26 +0200

    refactoring code

--- src/main/java/com/futurecollars/lesson15/task5/WordDisplayer.java
+++ src/main/java/com/futurecollars/lesson15/task5/WordDisplayer.java
@@ -3,17 +3,17 @@ package com.futurecollars.lesson15.task5;
 import java.util.Arrays;
 import java.util.List;
 
-public class WordDisplay {
+public class WordDisplayer {
 
     public static void displayWords(List<String> words) {
         words
                 .stream()
                 .map(o1 -> o1.split(""))
                 .flatMap(Arrays::stream)
-                .distinct()
                 .filter(x -> !x.contains(","))
                 .map(String::toUpperCase)
-                .forEach(System.out::print);
+                .distinct()
+                .forEach((k) -> System.out.print(k + " "));
     }
 
 }
