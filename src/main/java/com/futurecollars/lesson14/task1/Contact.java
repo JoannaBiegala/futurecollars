37a934904ad64e774ebd925b225c884dfbd7edcd
Joanna Biegala <joannabiegala0511@gmail.com>
2022-07-14 11:46:47 +0200

    refactoring code

--- src/main/java/com/futurecollars/lesson14/task1/Contact.java
+++ src/main/java/com/futurecollars/lesson14/task1/Contact.java
@@ -3,12 +3,17 @@ package com.futurecollars.lesson14.task1;
 public class Contact {
 
     private final String name;
-    private final String phoneNumber;
+    private String phoneNumber;
     private final String address;
 
     public Contact(String name, String phoneNumber, String address) {
         this.name = name;
         this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public Contact(String name, String address) {
+        this.name = name;
         this.address = address;
     }
 
--- src/main/java/com/futurecollars/lesson14/task1/PhoneBook.java
+++ src/main/java/com/futurecollars/lesson14/task1/PhoneBook.java
@@ -5,18 +5,19 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class PhoneBook {
 
     private static final String DATA_PATH = "src/main/java/com/futurecollars/lesson14/task1/data.txt";
-    Map<String, Contact> phoneBook;
+    private final Map<String, Contact> phoneBook;
 
     public PhoneBook() {
         phoneBook = new HashMap<>();
         loadContacts();
     }
 
-    void loadContacts() {
+    private void loadContacts() {
         String line;
         try (BufferedReader reader = new BufferedReader(new FileReader(DATA_PATH))) {
             while ((line = reader.readLine()) != null) {
@@ -28,19 +29,15 @@ public class PhoneBook {
 
                 phoneBook.put(name, contact);
             }
-        } catch (IOException ioex) {
+        } catch (IOException ioException) {
             System.err.println("Could not load contacts, phone book is empty!");
         }
     }
 
-    public Contact find(String name) throws RuntimeException {
-        Contact contact;
-        if (phoneBook.get(name) != null) {
-            contact = phoneBook.get(name);
-        } else {
-            throw new RuntimeException("There is no such person in the phone book");
-        }
-        return contact;
+    public Contact find(String name) {
+        return Optional
+                .ofNullable(phoneBook.get(name))
+                .orElseThrow(() -> new RuntimeException("There is no such person in the phone book: " + name));
     }
 
 }
