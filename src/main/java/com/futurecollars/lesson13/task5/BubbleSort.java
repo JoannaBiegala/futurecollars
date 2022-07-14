4814d545be11feab6606710180205c9d7ce88901
Joanna Biegala <joannabiegala0511@gmail.com>
2022-07-14 11:19:39 +0200

    refactoring code

--- src/main/java/com/futurecollars/lesson13/task5/BubbleSort.java
+++ src/main/java/com/futurecollars/lesson13/task5/BubbleSort.java
@@ -1,21 +1,11 @@
 package com.futurecollars.lesson13.task5;
 
-public class BubbleSort<E extends Comparable<E>> {
+public class BubbleSort {
 
-    E[] table;
-
-    public BubbleSort(E[] table) {
-        this.table = table;
-    }
-
-    public boolean compareTo(E e1, E e2) {
-        return e1.compareTo(e2) > 0;
-    }
-
-    public void bubbleSort() {
+    public static <E extends Comparable> void bubbleSort(E[] table) {
         for (int i = 0; i < table.length; i++) {
             for (int j = 0; j < table.length - 1; j++) {
-                if (compareTo(table[j], table[j + 1])) {
+                if (table[j].compareTo(table[j + 1]) > 0) {
                     E temp = table[j + 1];
                     table[j + 1] = table[j];
                     table[j] = temp;
@@ -23,5 +13,6 @@ public class BubbleSort<E extends Comparable<E>> {
             }
         }
     }
+
 
 }
--- src/main/java/com/futurecollars/lesson13/task5/Main.java
+++ src/main/java/com/futurecollars/lesson13/task5/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Integer[] table = {23, 55, 12, 78, 56, 33, 55};
-        BubbleSort<Integer> bubbleSort = new BubbleSort<>(table);
-        bubbleSort.bubbleSort();
+        BubbleSort.bubbleSort(table);
         for (int element : table) {
             System.out.println(element);
         }
