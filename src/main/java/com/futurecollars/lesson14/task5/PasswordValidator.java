375957cd01aa1662ea73a7b273b371b025868bc0
Joanna Biegala <joannabiegala0511@gmail.com>
2022-07-19 16:12:08 +0200

    refactoring code

--- src/main/java/com/futurecollars/lesson14/task5/PasswordValidator.java
+++ src/main/java/com/futurecollars/lesson14/task5/PasswordValidator.java
@@ -4,25 +4,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PasswordValidator {
+    private static final String REGEX = "^((?!21).)*((\\S*[A-Z]+\\S*\\d+)|(\\S*\\d+\\S*[A-Z]+\\S*)).{7,}$";
 
-    private static final String regexUpperCaseLetter = "[A-Z]+";
-    private static final String regexDigit = "\\d+";
-    private static final String regexYear = "(21)+";
-
-    public static boolean validatePassword(String password) {
-        if (password.length() < 7) {
-            return false;
-        } else if (!checkRegex(password, regexUpperCaseLetter)) {
-            return false;
-        } else if (!checkRegex(password, regexDigit)) {
-            return false;
-        } else return !checkRegex(password, regexYear);
-    }
-
-    private static boolean checkRegex(String password, String regex) {
-        Pattern pattern = Pattern.compile(regex);
+    static boolean validatePassword(String password) {
+        Pattern pattern = Pattern.compile(REGEX);
         Matcher matcher = pattern.matcher(password);
-        return matcher.find();
+         return matcher.matches();
     }
 
 }
