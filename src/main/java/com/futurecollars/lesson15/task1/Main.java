d94df8f0ba6f2472cece01b02ae928b5b982ca1a
Joanna Biegala <joannabiegala0511@gmail.com>
2022-07-20 08:30:58 +0200

    refactoring code

--- src/main/java/com/futurecollars/lesson15/task1/Main.java
+++ src/main/java/com/futurecollars/lesson15/task1/Main.java
@@ -3,7 +3,7 @@ package com.futurecollars.lesson15.task1;
 public class Main {
 
     public static void main(String[] args) {
-        NumbersDivisibleByThree.filterNumbersDivisibleByThree(-20, 40);
+        NumberOperations.printNumbersDivisibleByThree(-20, 40);
     }
 
 }
