30750f6f4a4816a629c52c560e2bce7ac5240bd3
Joanna Biegala <joannabiegala0511@gmail.com>
2022-07-21 11:52:00 +0200

    refactoring code

--- src/main/java/com/futurecollars/lesson13/task3/GenericList.java
+++ src/main/java/com/futurecollars/lesson13/task3/GenericList.java
@@ -1,29 +1,38 @@
 package com.futurecollars.lesson13.task3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenericList<E> implements OwnList<E> {
 
+    List<Object[]> elementsList;
     Object[] elements;
     private int size = 0;
 
     public GenericList() {
-        this.elements = new Object[DEFAULT_CAPACITY];
+       elements = new Object[DEFAULT_CAPACITY];
+       elementsList = new ArrayList<>();
     }
 
     @Override
     public boolean add(E o) {
-        if (size() > DEFAULT_CAPACITY) {
-            return false;
-        } else {
+        if (size() >= DEFAULT_CAPACITY) {
+            Object[] fullArrayOfObjects = elements.clone();
+            elementsList.add(fullArrayOfObjects);
+            elements = new Object[DEFAULT_CAPACITY];
+            elements[0]=o;
+            size = 1;
+         } else {
             elements[size()] = o;
             size++;
-            return true;
         }
+        return true;
     }
 
     @Override
     public E get(int index) {
         if (index > size()) {
-            return null;
+             throw new IndexOutOfBoundsException();
         } else {
             return (E) elements[index];
         }
