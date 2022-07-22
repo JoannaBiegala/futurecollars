8a9cd353d9afd7424955c302677c63218b195acc
Joanna Biegala <joannabiegala0511@gmail.com>
2022-07-22 13:58:43 +0200

    refactoring code

--- src/main/java/com/futurecollars/lesson13/task3/GenericList.java
+++ src/main/java/com/futurecollars/lesson13/task3/GenericList.java
@@ -1,32 +1,27 @@
 package com.futurecollars.lesson13.task3;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class GenericList<E> implements OwnList<E> {
 
-    List<Object[]> elementsList;
     Object[] elements;
-    private int sizeCurrentArray = 0;
+    private int defaultCapacity = DEFAULT_CAPACITY;
+    int size = 0;
 
     public GenericList() {
-        elements = new Object[DEFAULT_CAPACITY];
-        elementsList = new ArrayList<>();
-        elementsList.add(elements);
+        this.elements = new Object[DEFAULT_CAPACITY];
     }
 
     @Override
     public boolean add(E o) {
-        if (sizeCurrentArray >= DEFAULT_CAPACITY) {
-            elements = new Object[DEFAULT_CAPACITY];
-            elementsList.add(elements);
-            elements[0] = o;
-            sizeCurrentArray = 1;
-        } else {
-            elements[sizeCurrentArray] = o;
-            sizeCurrentArray++;
+        if (size() >= defaultCapacity) {
+            defaultCapacity = defaultCapacity + DEFAULT_CAPACITY;
+            Object[] elementsCopy = elements.clone();
+            elements = new Object[defaultCapacity];
+            System.arraycopy(elementsCopy, 0, elements, 0, elementsCopy.length);
         }
+        elements[size()] = o;
+        size++;
         return true;
+
     }
 
     @Override
@@ -34,33 +29,28 @@ public class GenericList<E> implements OwnList<E> {
         if (index > size()) {
             throw new IndexOutOfBoundsException();
         } else {
-                int indexInElementsList = index / DEFAULT_CAPACITY;
-                int positionOfElements = index % DEFAULT_CAPACITY;
-                return (E) elementsList.get(indexInElementsList)[positionOfElements];
+            return (E) elements[index];
         }
     }
 
     @Override
     public boolean remove(E o) {
         for (int index = 0; index < size(); index++) {
-            int indexInElementsList = index / DEFAULT_CAPACITY;
-            int positionOfElements = index % DEFAULT_CAPACITY;
-            if (elementsList.get(indexInElementsList)[positionOfElements] == o) {
-               if (indexInElementsList == 0) {
-                   if (size() - index >= 0) System.arraycopy(elements, index + 1, elements, index, size() - index);
-                   elements[size() - 1] = null;
-                   sizeCurrentArray--;
-                   return true;
-               }
+            if (elements[index] == o) {
+                if (size() - index >= 0) {
+                    System.arraycopy(elements, index + 1, elements, index, size() - index);
+                }
+                elements[size() - 1] = null;
+                size--;
+                return true;
             }
-
         }
         return false;
     }
 
     @Override
     public int size() {
-        return (elementsList.size() - 1) * 10 + sizeCurrentArray;
+        return size;
     }
 
 }
