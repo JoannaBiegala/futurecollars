b0ee9cbcfb1a10a5b35307f2f11ead39a102a353
Joanna Biegala <joannabiegala0511@gmail.com>
2022-07-22 08:42:21 +0200

    refactoring code

--- src/main/java/com/futurecollars/lesson14/task5/PasswordValidator.java
+++ src/main/java/com/futurecollars/lesson14/task5/PasswordValidator.java
@@ -1,15 +1,13 @@
 package com.futurecollars.lesson14.task5;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PasswordValidator {
     private static final String REGEX = "^((?!21).)*((\\S*[A-Z]+\\S*\\d+)|(\\S*\\d+\\S*[A-Z]+\\S*)).{7,}$";
+    private static final Pattern pattern = Pattern.compile(REGEX);
 
-    static boolean validatePassword(String password) {
-        Pattern pattern = Pattern.compile(REGEX);
-        Matcher matcher = pattern.matcher(password);
-         return matcher.matches();
+    public static boolean validatePassword(String password) {
+        return pattern.matcher(password).matches();
     }
 
 }
